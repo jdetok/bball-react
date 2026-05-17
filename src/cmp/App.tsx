e15a3bfe137b2ef4e165483e9b8349ee904f1e7f
jdetok <jdeko17@gmail.com>
2026-05-17 16:02:13 -0500

    basic react table component working well

--- src/cmp/App.tsx
+++ src/cmp/App.tsx
@@ -1,7 +1,59 @@
+import Table from "./tbl/Table";
+import data from '../../data/api/recentGames.json';
+import data1 from '../../data/api/teamRecords.json';
+import playerData from '../../data/api/playerSearch.json';
+
+const nbaTeamRecs = data1.nba_team_records;
+const wnbaTeamRecs = data1.wnba_team_records;
+const boxTots = playerData.player[0]?.totals.box_stats;
+const boxAvgs = playerData.player[0]?.per_game.box_stats;
+const shtAvgs = playerData.player[0]?.per_game.shooting;
+const shtTots = playerData.player[0]?.totals.shooting;
+const player = playerData.player[0];
+const playerMeta = player?.player_meta;
+
 export default function App() {
     return (
         <main className="app">
-            <h1>Hello World</h1>
+            <Table
+                cssClass="tbl"
+                type="ranked"
+                header={`Top Scorers from ${data.recent_games[0]?.game_date}`}
+                data={data.top_scorers}
+                excludeFields={['player_id', 'team_id', 'league']}
+                rowHeaderFields={['player']}
+            />
+            <Table
+                cssClass="tbl"
+                type="ranked"
+                header={`${nbaTeamRecs[0]?.league} Team Records: ${nbaTeamRecs[0]?.season_desc}`}
+                data={nbaTeamRecs}
+                excludeFields={['league', 'season_id', 'season_desc', 'season', 'team_id', 'team']}
+                rowHeaderFields={['team_long']}
+            />
+            <Table
+                cssClass="tbl"
+                type="ranked"
+                header={`${wnbaTeamRecs[0]?.league} Team Records: ${wnbaTeamRecs[0]?.season_desc}`}
+                data={wnbaTeamRecs}
+                excludeFields={['league', 'season_id', 'season_desc', 'season', 'team_id', 'team']}
+                rowHeaderFields={['team_long']}
+            />
+
+            <div>
+                <Table
+                    cssClass="tbl"
+                    type="basic"
+                    header={`${playerMeta?.player} ${playerMeta?.cap_box_tot}`}
+                    data={[boxTots]}
+                />
+                <Table
+                    cssClass="tbl"
+                    type="basic"
+                    header={`${playerMeta?.player} ${playerMeta?.cap_box_avg}`}
+                    data={[boxAvgs]}
+                />
+            </div>
         </main>
     )
 }
---
+++ src/cmp/tbl/Table.tsx
@@ -0,0 +1,45 @@
+export type tableProps = {
+    header: string;
+    cssClass: string;
+    data: any[];
+    type: 'basic' | 'ranked'; // assigned as table element's class
+    excludeFields?: string[];
+    rowHeaderFields?: string[]; // render these fields as th rather than td
+}
+
+export default function Table({ header, cssClass, type, data, excludeFields, rowHeaderFields }: tableProps) {
+    // keys of first object in array are the headers
+    const headers = (Object.keys(data[0])
+        .filter((k) => !excludeFields?.includes(k)))
+        .map((s) => `${s.charAt(0).toUpperCase()}${s.slice(1)}`);
+
+    // new array of objects only wiith the fields with headers
+    const filtered = data.map((obj) => Object.fromEntries(Object.entries(obj)
+        .filter(([k]) => !excludeFields?.includes(k))));
+
+    return (
+        <div className={`${cssClass}`}>
+            <div className='ttl'>{header}</div>
+            <table className={type}>
+                <thead>
+                    <tr>
+                        {type === 'ranked'  && <th>Rank</th>}
+                        {headers.map((h, i) => <th key={`header-${i}`}>{h}</th>)}
+                    </tr> 
+                </thead>
+                <tbody>
+                    {filtered.map((obj, rowIdx) => (
+                        <tr>
+                            {type === 'ranked' && <th>{rowIdx + 1}</th>}
+                            {Object.entries(obj).map(([k, v]: any, i) => (
+                                rowHeaderFields?.includes(k)
+                                    ? <th key={`val-${i}`}>{v}</th>
+                                    : <td key={`val-${i}`}>{v}</td>
+                            ))}
+                        </tr>
+                    ))}
+                </tbody>
+            </table>
+        </div>
+    )
+}
